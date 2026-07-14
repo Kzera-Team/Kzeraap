baf063437a540ae5fa074d119410e1ea538bc572
jjjtestejoao-ui <jjjtestejoao@gmail.com>
2026-07-14 11:16:38 -0300

    feat(fidelidade): expose domain contracts

---
+++ src/domain/fidelidade/index.ts
@@ -0,0 +1,7 @@
+export * from './FidelidadeErrors';
+export * from './RegraFidelidade';
+export * from './CalculadoraPontos';
+export * from './ContaFidelidade';
+export * from './MovimentacaoFidelidade';
+export * from './LotePontos';
+export * from './AuditoriaFidelidade';
